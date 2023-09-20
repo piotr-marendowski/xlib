d3f00e248fa6d81767bf018cadfac3dad29cd8a4
Piotr Marendowski <piotr-marendowski@tutanota.com>
2023-09-20 14:25:32 +0000

    Add programs

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # xlib
-Examples of using the Xlib library
+
+### Most of them are from Guy Keren's ["Basic Graphics Programming With The Xlib Library"](https://ftp.dim13.org/pub/doc/Xlib.pdf).
+
+### simple-drawing.c
+
+![simple-drawing](assets/simple-drawing.png)
+
+### events.c
+
+![events](assets/events.png)
+
+### simple-text.c
+
+![simple-text](assets/simple-text.png)
+
+### simple-wm-hints.c
+
+Integrates well with dwm, but I don't have patch allowing me to display icons. On Qtile it doesn't show icon either.
+
+![simple-wm-hints](assets/simple-wm-hints.png)
+
+### Compile and run
+
+```
+gcc -g -o out simple-drawing.c -lX11 && ./out
+```
+
+Don't see any need for a Makefile.
---
+++ simple-drawing.c
@@ -0,0 +1,126 @@
+#include <X11/Xlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+Window
+create_simple_window(Display* display, int width, int height, int x, int y)
+{
+    int screen_num = DefaultScreen(display);
+    int win_border_width = 2;
+    Window win;
+
+    win = XCreateSimpleWindow(display, RootWindow(display, screen_num), x, y, width,
+                              height, win_border_width,
+                              BlackPixel(display, screen_num),
+                              WhitePixel(display, screen_num));
+
+    XMapWindow(display, win);
+
+    XFlush(display);
+
+    return win;
+}
+
+
+GC
+create_gc(Display* display, Window win, int reverse_video)
+{
+    GC gc;
+    unsigned long valuemask = 0;
+    XGCValues values;
+    unsigned int line_width = 2;
+    int line_style = LineSolid;
+    int cap_style = CapButt;
+    int join_style = JoinBevel;
+    int screen_num = DefaultScreen(display);
+
+    gc = XCreateGC(display, win, valuemask, &values);
+
+    if (gc < 0) {
+        fprintf(stderr, "XCreateGC: \n");
+    }
+
+    if (reverse_video) {
+        XSetForeground(display, gc, WhitePixel(display, screen_num));
+        XSetBackground(display, gc, BlackPixel(display, screen_num));
+    }
+    else {
+        XSetForeground(display, gc, BlackPixel(display, screen_num));
+        XSetBackground(display, gc, WhitePixel(display, screen_num));
+    }
+ 
+    XSetLineAttributes(display, gc,
+                       line_width, line_style, cap_style, join_style);
+
+    XSetFillStyle(display, gc, FillSolid);
+       
+    return gc;
+}
+
+int main(int argc, char* argv[]) {
+    Display* display;
+    int screen_num;
+    Window win;
+    unsigned int display_width, display_height;
+    unsigned int width, height;    
+    char *display_name = getenv("DISPLAY");
+    GC gc;
+
+    display = XOpenDisplay(display_name);
+    if (display == NULL) {
+        fprintf(stderr, "%s: cannot connect to X server '%s'\n",
+                argv[0], display_name);
+        exit(1);
+    }
+
+    screen_num = DefaultScreen(display);
+    display_width = DisplayWidth(display, screen_num);
+    display_height = DisplayHeight(display, screen_num);
+
+    width = (display_width / 3);
+    height = (display_height / 3);
+    printf("window width - '%d'; height - '%d'\n", width, height);
+
+    win = create_simple_window(display, width, height, 0, 0);
+    
+    gc = create_gc(display, win, 0);
+    XSync(display, False);
+
+    /* to display them until window is closed */
+    while (1) {
+        XDrawPoint(display, win, gc, 5, 5);
+        XDrawPoint(display, win, gc, 5, height-5);
+        XDrawPoint(display, win, gc, width-5, 5);
+        XDrawPoint(display, win, gc, width-5, height-5);
+
+        XDrawLine(display, win, gc, 50, 0, 50, 200);
+        XDrawLine(display, win, gc, 0, 100, 200, 100);
+
+        XDrawArc(display, win, gc, 50-(30/2), 100-(30/2), 30, 30, 0, 360*64);
+
+        {
+            XPoint points[] = {
+                {0, 0},
+                {15, 15},
+                {0, 15},
+                {0, 0}
+            };
+            int npoints = sizeof(points)/sizeof(XPoint);
+
+            XDrawLines(display, win, gc, points, npoints, CoordModeOrigin);
+        }
+
+        XDrawRectangle(display, win, gc, 120, 150, 50, 60);
+
+        XFillRectangle(display, win, gc, 60, 150, 50, 60);
+
+        XFlush(display);
+        XSync(display, False);
+    }
+
+    XCloseDisplay(display);
+
+    return 0;
+}
